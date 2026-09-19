92a54e7529806c662f45dd2ca6662ed425fd4bee
musheabdulhakim <musheabdulhakim99@gmail.com>
2026-09-19 19:36:52 +0000

    feat: place the website cluster in the Administration sidebar group

--- app/Filament/Clusters/Website/WebsiteCluster.php
+++ app/Filament/Clusters/Website/WebsiteCluster.php
@@ -5,17 +5,17 @@ namespace Modules\Website\Filament\Clusters\Website;
 use BackedEnum;
 use Filament\Clusters\Cluster;
 use Filament\Support\Icons\Heroicon;
-use Modules\Core\Enums\NavigationGroup;
+use Modules\Core\Enums\SidebarGroup;
 
 class WebsiteCluster extends Cluster
 {
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedGlobeAlt;
 
-    protected static string|\UnitEnum|null $navigationGroup = NavigationGroup::ADMINISTRATION;
+    protected static string|\UnitEnum|null $navigationGroup = SidebarGroup::Administration;
 
     protected static ?string $navigationLabel = 'Website';
 
-    protected static ?int $navigationSort = 40;
+    protected static ?int $navigationSort = 20;
 
     protected static bool $shouldRegisterSubNavigation = true;
 }
